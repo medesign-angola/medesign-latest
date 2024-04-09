21df0f7f146ca2dc1b474d365d14dafb5877dd08
IsaquiasMarques <patisaquias2000@gmail.com>
2024-04-09 11:31:48 +0100

    envi prod variables not working

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    api: 'http://medesign-ao.host/wp-json',
+    api: 'https://medesignao.medesign-angola.com/wp-json',
     newsletter: {
         clientKey: 'a066bbd314bff737db8fa8d7856cf87af175b1c9',
         clientSecret: '071bed68d86675506f85e6bbeae85b2708af9099'
